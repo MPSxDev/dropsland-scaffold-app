4a4f64c03289ba53abfe71d8e58dd45428bab7b6
MPSxDev <mparedes.digital@gmail.com>
2025-11-08 17:12:41 -0300

    feat: add profile type management system
    - Add ProfileTypeProvider for managing DJ/Fan profile types
    - Add useProfileType hook for accessing profile type context
    - Add profileType to storage schema
    - Integrate ProfileTypeProvider in app root

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import "@stellar/design-system/build/styles.min.css";
 import { WalletProvider } from "./providers/WalletProvider.tsx";
 import { NotificationProvider } from "./providers/NotificationProvider.tsx";
+import { ProfileTypeProvider } from "./providers/ProfileTypeProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -22,9 +23,11 @@ createRoot(document.getElementById("root") as HTMLElement).render(
     <NotificationProvider>
       <QueryClientProvider client={queryClient}>
         <WalletProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+          <ProfileTypeProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </ProfileTypeProvider>
         </WalletProvider>
       </QueryClientProvider>
     </NotificationProvider>
